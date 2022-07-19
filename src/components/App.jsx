c00965bd66e8052bc62a7dadda99108332402bf4
Anastasiia <41988936+balanasia@users.noreply.github.com>
2022-07-19 13:25:07 -0500

    Organize code into imports. Add styles to the elements inside the jsx files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,20 @@
+//1. Apply CSS styles to App.jsx component
+//to match the appearance on the completed app:
+//https://c6fkx.csb.app/
+//2. Extract the contact card as a reusable Card component.
+//3. Use props to render
+
 import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <Card {...contacts[0]} />
+      <Card {...contacts[1]} />
+      <Card {...contacts[2]} />
     </div>
   );
 }
